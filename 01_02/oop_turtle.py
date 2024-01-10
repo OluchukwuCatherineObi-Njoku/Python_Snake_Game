b9c6dbb58332810776a60277221efd3e9082a494
Oluchukwu Catherine Obi-Njoku <oobi-njoku@myseneca.ca>
2024-01-10 11:15:27 -0500

    Start_with_turtle

---
+++ 01_02/oop_turtle.py
@@ -0,0 +1,14 @@
+import turtle # import the turtle module
+
+# create a new turtle object
+
+my_turtle = turtle.Turtle() # create a new turtle object 
+    #turtle is a module, Turtle is a class
+my_turtle.shape("turtle") # set the shape to a turtle
+my_turtle.color("red") # set the color to red
+
+# move the turtle forward 100 pixels
+my_turtle.forward(100) # move forward 100 pixels
+
+turtle.done() # keep the window open until it is closed manually
+
---
+++ 01_02/procedural.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+color("red")
+forward(100) # move forward 100 pixels
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+
+done()
+#draws a square
